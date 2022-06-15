f92c8d28da04953fcef0c52fc992469675b691bf
vDesign <vDesign@vD-iMac.local>
2022-06-14 20:19:31 -0700

    Add html5 landmarks
    • main
    • footer

--- index.php
+++ index.php
@@ -150,7 +150,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
   </div>
   <!--END - TAG CANVAS-->  
   </div>
-  <div data-w-id="f44b0118-88b7-9ca9-cce2-b3a39459ab86" class="page">
+  <main data-w-id="f44b0118-88b7-9ca9-cce2-b3a39459ab86" class="page">
     <div id="about" class="about">
       <h2 class="section-heading">ABOUT</h2>
       <p class="about-paragraph">Vic is an experienced User Experience Specialist with a demonstrated history of working in the internet industry, Skilled in UX Research, Strategy, Design Thinking, Value Proposition Design, Flow Diagrams, Wire-framing, Prototyping, Front End Engineering, and Art Direction. A generalist within the user-centered design process, Vic delivers engaging user experiences through research, feedback, analysis, testing, and best practices.</p>
@@ -337,8 +337,8 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
         <div class="w-slider-nav w-round"></div>
       </div>
     </div>
-  </div>
-  <div class="footer">
+  </main>
+  <footer class="footer">
     <div id="contact" class="contact">
       <h2 class="section-heading">CONNECT</h2>
       <div class="social-media-links">
@@ -397,7 +397,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <img src="images/logo_2-white.png" alt="VARCXX Trademark Symbol" class="image-4">
      </div>
     </div>
-  </div>
+  </footer>
   <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=5b0c51bb15edac23aebf97a5" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
   <script src="js/webflow.js" type="text/javascript"></script>
 
